fb0dccc7166b76b276a235b524a57369b43dff79
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-11-05 22:12:34 +0100

    Fixed type on register

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import User from "@/models/Userr";
+import User from "@/models/User";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
